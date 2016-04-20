28212ce1fe11269aedc9d10918551c14eef45dc6
jsgalaska <jsgalaska@gmail.com>
2016-04-19 20:37:47 -0400

    Change Os to Xs

--- app/src/main/java/com/example/mega/othello/OthelloGame.java
+++ app/src/main/java/com/example/mega/othello/OthelloGame.java
@@ -1,9 +1,12 @@
 package com.example.mega.othello;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridLayout;
+import android.widget.TextView;
 
 public class OthelloGame extends AppCompatActivity {
 
@@ -15,5 +18,14 @@ public class OthelloGame extends AppCompatActivity {
         Intent intent = getIntent();
 
         GridLayout layout = (GridLayout) findViewById(R.id.othelloGrid);
+    }
+
+    public void registerClick(View view){
+        TextView t = (TextView) view;
+        if(("O").equals(t.getText())){
+            t.setText("X");
+        }else{
+            t.setText("O");
+        }
     }
 }
