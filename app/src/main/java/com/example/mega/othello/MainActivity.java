7e98d803dcee75abf70de781c06b0616dacb09b2
DAMedina <vipergtsr009@hotmail.com>
2016-04-28 15:46:20 -0400

    Super basic implementation of sound for button/s

--- app/src/main/java/com/example/mega/othello/MainActivity.java
+++ app/src/main/java/com/example/mega/othello/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.mega.othello;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* sound for button (super basic)*/
+    MediaPlayer menuSound;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* sound for button (super basic)*/
+        menuSound = MediaPlayer.create(this, R.raw.menu_select0);
+
     }
 
     public void launchNewGame(View view){
         Intent intent = new Intent(this, OthelloGame.class);
         startActivity(intent);
+
+        /* sound for button (super basic)*/
+        menuSound.start();
     }
 }
