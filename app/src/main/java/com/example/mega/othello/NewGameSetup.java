34c57bf21c900f21a9fbe5c790ab9c2fffaeba05
DAMedina <vipergtsr009@hotmail.com>
2016-05-02 19:39:28 -0400

    Some Edits & New screen for pre-game setup

--- app/src/main/java/com/example/mega/othello/NewGameSetup.java
+++ app/src/main/java/com/example/mega/othello/NewGameSetup.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class NewGameSetup extends AppCompatActivity {
 
     // sound for button
     MediaPlayer menuSound;
@@ -14,18 +14,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_game_setup);
 
         // sound for button
         menuSound = MediaPlayer.create(this, R.raw.menu_select0);
-
     }
 
-    public void launchNewGame(View view){
-        Intent intent = new Intent(this, NewGameSetup.class);
+    public void launchGame(View view){
+        Intent intent = new Intent(this, OthelloGame.class);
         startActivity(intent);
-
-        // sound for button
-        menuSound.start();
+        menuSound.start(); // sound for button
+    }
+    public void returnToMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        menuSound.start(); // sound for button
     }
 }
